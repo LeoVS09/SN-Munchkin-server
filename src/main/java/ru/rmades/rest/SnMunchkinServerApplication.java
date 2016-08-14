6227524292f59633a077cf5e13d60aa6698b07e2
LeoVS09 <LeoVS09@mail.ru>
2016-08-14 17:53:10 +0300

    Create project

---
+++ src/main/java/ru/rmades/rest/SnMunchkinServerApplication.java
@@ -0,0 +1,12 @@
+package ru.rmades.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnMunchkinServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnMunchkinServerApplication.class, args);
+	}
+}
---
+++ src/test/java/ru/rmades/rest/SnMunchkinServerApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.rmades.rest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SnMunchkinServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
