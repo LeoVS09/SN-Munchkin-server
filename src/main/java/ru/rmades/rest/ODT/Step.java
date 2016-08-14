02687cc5a8c55be99e41347a14ee4225c442fcaa
LeoVS09 <LeoVS09@mail.ru>
2016-08-14 23:01:56 +0300

    Create objects of database

---
+++ src/main/java/ru/rmades/rest/ODT/Step.java
@@ -0,0 +1,13 @@
+package ru.rmades.rest.ODT;
+
+/**
+ * Created by Администратор on 14.08.2016.
+ */
+public class Step {
+    short x;
+    short y;
+    public Step(short x, short y){
+        this.x = x;
+        this.y = y;
+    }
+}
